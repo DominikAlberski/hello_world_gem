30cea1952233f4fe4c6aac2cf1ae7a396253916f
Dominik <dominik.alberski@gmail.com>
2024-04-03 14:34:01 +0200

    add file require

--- lib/hello_world_gem.rb
+++ lib/hello_world_gem.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "hello_world_gem/version"
+require_relative "hello_world_gem/hello_world"
 
 module HelloWorldGem
   class Error < StandardError; end
